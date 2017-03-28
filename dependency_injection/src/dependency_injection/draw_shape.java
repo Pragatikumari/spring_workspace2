23ea8fafa426af7947e9d35a896d115686b8522b
pragati <pragatisharma062 @gmail.com>
2017-03-28 08:54:42 +0530

    dependency_injection implemented

---
+++ dependency_injection/src/dependency_injection/draw_shape.java
@@ -0,0 +1,15 @@
+package dependency_injection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class draw_shape {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+	       Triangle triangle= (Triangle)context.getBean("triangle");
+	       triangle.draw();
+
+	}
+
+}
