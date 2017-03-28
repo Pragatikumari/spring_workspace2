192fcc59151b2427b913df5b0620a5d5218eb631
pragati <pragatisharma062 @gmail.com>
2017-03-28 09:42:16 +0530

    intiase a datamember of type list

---
+++ intialise_List_datamember/src/intialise_List_datamember/Point.java
@@ -0,0 +1,26 @@
+package intialise_List_datamember;
+public class Point {
+	private int X;
+	private int Y;
+	
+	
+	public int getX() {
+		return X;
+	}
+	
+	public void setX(int x) {
+		X = x;
+	}
+	
+	
+	public int getY() {
+		return Y;
+	}
+	
+	public void setY(int y) {
+		Y = y;
+	}
+	
+	
+
+}
