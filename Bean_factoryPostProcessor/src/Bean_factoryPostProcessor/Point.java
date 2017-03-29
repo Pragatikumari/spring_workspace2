6c41bb090cdd15cd1af14b873fce9d917f464bb6
pragati <pragatisharma062 @gmail.com>
2017-03-30 00:42:45 +0530

    example of bean factory post processor

---
+++ Bean_factoryPostProcessor/src/Bean_factoryPostProcessor/Point.java
@@ -0,0 +1,25 @@
+package Bean_factoryPostProcessor;
+
+public class Point {
+	
+
+	private int X,Y;
+
+	public int getX() {
+		return X;
+	}
+
+	public void setX(int x) {
+		X = x;
+	}
+
+	public int getY() {
+		return Y;
+	}
+
+	public void setY(int y) {
+		Y = y;
+	}
+
+
+}
---
+++ Bean_factoryPostProcessor/src/Bean_factoryPostProcessor/draw_shape.java
@@ -0,0 +1,18 @@
+package Bean_factoryPostProcessor;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+ 
+public class draw_shape {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+	       // for closing the context created comes with AbstractApplicationContext
+	 
+ Triangle triangle= (Triangle)context.getBean("triangle");
+ triangle.draw();
+
+	}
+
+}
